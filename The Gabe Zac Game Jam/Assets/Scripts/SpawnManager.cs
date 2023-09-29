b71775565bf16c2f73d28a8cb860996da5772c96
guccigeg <gregshipley19@gmail.com>
2023-09-28 19:06:31 -0600

    gegs final changes

--- The Gabe Zac Game Jam/Assets/Scripts/SpawnManager.cs
+++ The Gabe Zac Game Jam/Assets/Scripts/SpawnManager.cs
@@ -20,7 +20,7 @@ public class SpawnManager : MonoBehaviour
     private IEnumerator spawnEnemy (float interval, GameObject enemy)
     {
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-20, 20), Random.Range(-20, 20), 18), Quaternion.identity);
         StartCoroutine(spawnEnemy(interval, enemy));
     }
 }
