668763268c1a0a8a5f12ad48c418f93e0efd67a5
zacwsh <146384547+zacwsh@users.noreply.github.com>
2023-09-29 13:44:15 -0600

    Big Changes

--- The Gabe Zac Game Jam/Assets/Scripts/Bullet.cs
+++ The Gabe Zac Game Jam/Assets/Scripts/Bullet.cs
@@ -14,7 +14,15 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Destroy(collision.gameObject);
-        Destroy(gameObject);
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+        else
+        {
+            return;
+        }
+
     }
 }
