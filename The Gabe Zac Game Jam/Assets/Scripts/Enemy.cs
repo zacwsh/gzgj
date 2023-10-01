d21b4044113543766fe2c01767630de5b2b36f02
gabeslade <gabesladebusiness@gmail.com>
2023-10-01 11:13:36 -0600

    Made enemy ai and player can die now

--- The Gabe Zac Game Jam/Assets/Scripts/Enemy.cs
+++ The Gabe Zac Game Jam/Assets/Scripts/Enemy.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public static event Action<Enemy> OnEnemyKilled;
+    [SerializeField] float health, maxHealth = 3f;
+
+    [SerializeField] float moveSpeed = 12f;
+    Rigidbody rb;
+    Transform target;
+    Vector3 moveDirection;
+
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = maxHealth;
+        target = GameObject.Find("FirstPersonController").transform;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            moveDirection = direction;
+        }
     }
+
+    private void FixedUpdate()
+    {
+        if (target)
+        {
+            rb.velocity = new Vector3(moveDirection.x, 0, moveDirection.z) * moveSpeed;
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
 }
