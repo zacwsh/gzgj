728e636aeb25d4352dfff88e6388ea9d2b4c8df4
zacwsh <146384547+zacwsh@users.noreply.github.com>
2023-10-01 16:35:16 -0600

    sfx

--- The Gabe Zac Game Jam/Assets/Scripts/Gun.cs
+++ The Gabe Zac Game Jam/Assets/Scripts/Gun.cs
@@ -8,14 +8,20 @@ public class Gun : MonoBehaviour
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
     public float bulletSpeed = 200;
+    public AudioClip soundEffect;
+    private AudioSource audioSource;
 
-
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
             bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+            audioSource.PlayOneShot(soundEffect);
         }
 
     }
